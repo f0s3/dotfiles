0d0c4fd9885ed47b25f785a28da3bea1949253a4
f0s3 <power.aerozoll@gmail.com>
2019-08-21 23:01:14 +0300

    Introduce sample install.sh

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# i3wm
+
+# polybar
+
+# fonts
+
+# login screen
+
+# terminal
+
+# my i3wm, polybar, termite configs
+
+# connmanctl-dmenu
+
+# i3-autokeymap
+
+# create ~/Projects folder
+
